e706def2095614288d43ce77a1a70e5004997811
werner20022 <werner20fbr@gmail.com>
2024-12-05 00:41:45 -0600

    se subio

---
+++ inc/footer.php
@@ -0,0 +1,47 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+
+        
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+
+
+
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    En Nicole lee, no solo vendemos zapatos, ¡vendemos experiencias! Cada producto cuenta una historia única y te invitamos a ser parte de ella..</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">MEXICO</p>
+                <p style="color: #FFF">Tlapa</p>
+                <p style="color: #FFF">Colonia Centro/Calle aldama/ #9</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  7571151671</p> <a href="#" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; Nicolelee@gmail.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">Nicole lee &copy; 2024</h5>
+</footer>
